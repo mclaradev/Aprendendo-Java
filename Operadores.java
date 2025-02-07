17bd34b8c1da054399beb298e17ca4161ba482bb
flora <mclaradev@gmail.com>
2025-02-07 09:10:09 -0300

    Exercícios básicos de Java

---
+++ Operadores.java
@@ -0,0 +1,12 @@
+public class Operadores {
+    public static void main(String[] args){
+        int numero = 5;
+
+        numero += 3;
+        //numero -= 2;
+        //numero = *= 4
+        //numero /= 2;
+
+        System.out.println(numero);
+    }
+}
