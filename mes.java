3a070442d8d9c47bef02a9f6e935419d9506dd2a
flora <mclaradev@gmail.com>
2025-02-14 15:31:51 -0300

    exercicio java de switch case bem simples

---
+++ mes.java
@@ -0,0 +1,36 @@
+public class mes {
+    public static void main(String []args) {
+        int mes = 1;
+        String resultado;
+
+        switch (mes){
+            case 1 -> resultado = "Janeiro";
+
+            case 2 -> resultado = "Fevereiro";
+
+            case 3 -> resultado = "Março";
+
+            case 4 -> resultado = "Abril";
+
+            case 5 -> resultado = "Maio";
+
+            case 6 -> resultado = "Julho";
+
+            case 7 -> resultado = "Junho";
+
+            case 8 -> resultado = "Agosto";
+
+            case 9 -> resultado = "Setembro";
+
+            case 10 -> resultado = "Outubro";
+
+            case 11 -> resultado = "Novembro";
+
+            case 12 -> resultado = "Dezembro";
+
+            default -> resultado = "Esse mês não existe não amigão kk";
+        }
+
+        System.out.println(resultado);
+    }
+}
