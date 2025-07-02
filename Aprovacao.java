d358f1fa5de80ebc88430c3a818df55cacee3cb3
flora <mclaradev@gmail.com>
2025-07-02 13:10:00 -0300

    mais exemplos de operadores lógicos em questão da aprovaçaão dos alunos por nota e presença em aula.

--- Aprovacao.java
+++ Aprovacao.java
@@ -2,11 +2,14 @@ public class Aprovacao {
     public static void main(String[]args){
         int nota1 = 10;
         int nota2 = 7;
+        int frequencia = 75;
 
         float media = (nota1 + nota2) / 2.0f;
 
-        if (media > 6) {
-            System.out.println("média: " + media + " você foi aprovado!");
+        if (media > 7 && frequencia >= 75) {
+            System.out.println("média: " + media);
+            System.out.println("Frequência: " + frequencia + "%");
+            System.out.println("Parabéns, você foi aprovado!");
         } else {
             System.out.println("média: " + media + " você foi reprovado");
         }
