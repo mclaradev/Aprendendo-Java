e021e66ef1d76260ded1ab731086f6b22566aac1
flora <mclaradev@gmail.com>
2025-07-04 10:37:02 -0300

    Ex de while - Contador de doces.

---
+++ ContandorDoces.java
@@ -0,0 +1,13 @@
+public class ContandorDoces {
+    public static void main(String[]args){
+        int doces = 1;
+
+        while(doces <= 3){
+            System.out.println(doces);
+
+            doces++;
+        }
+
+        System.out.println("Você comeu doce o suficiente.");
+    }
+}
